4a13bc30404773ddb5436e1697b31a620aed166d
TariqKichawele <tariqkichawele66@gmail.com>
2025-06-18 12:31:38 -0400

    Add initial project structure with components, styles, and hooks
    - Created `components.json` for configuration.
    - Added new dependencies in `package.json` and `package-lock.json`.
    - Implemented UI components including `Button`, `Input`, `Separator`, `Sheet`, `Sidebar`, and `Tooltip`.
    - Introduced global styles in `globals.css` with Tailwind CSS and custom properties.
    - Developed hooks for mobile and dark mode detection.
    - Set up middleware for Clerk authentication.
    - Established layout and sidebar structure for the application.
    - Enhanced the main page with sidebar integration and responsive design.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
+import { 
+  Sidebar, 
+  SidebarContent, 
+  SidebarHeader, 
+  SidebarProvider, 
+  SidebarTrigger,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarFooter,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSidebarClient";
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Smart Hire</span>
+          </SidebarHeader>
+          
+          <SidebarContent>
+            fefwfew
+          </SidebarContent>
+
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  fgfdg
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+
+        <main className="flex-1">
+          dsfrefr
+        </main>
+      </AppSidebarClient>
+    </SidebarProvider>
   );
 }
